6aa40646aa171e37a01db60d3506edc293a8f00c
Israel Castro <israel.castro@gmail.com>
2024-11-17 11:48:20 -0600

    Add new utility functions and refactor scripts
    Added new utility functions in `numbers.ts` for formatting subscriber counts. Refactored the Subscribe component and Theme toggling to use external scripts, improving code separation and maintainability. Adjusted styles in various components for better consistency.

---
+++ src/scripts/subscribeSendy.ts
@@ -0,0 +1,46 @@
+import { API_URL_SENDY, API_KEY_SENDY, LIST_ID_SENDY } from "astro:env/client";
+
+const form = document.getElementById("subscribeForm") as HTMLFormElement;
+const messageContainer = document.getElementById("message") as HTMLElement;
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const data = {
+    api_key: API_KEY_SENDY,
+    list: LIST_ID_SENDY,
+    email: formData.get("email")?.toString() || "",
+    boolean: "true",
+  };
+
+  try {
+    const response = await fetch(`${API_URL_SENDY}/subscribe`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams(data),
+    });
+
+    if (response.ok) {
+      const responseText = await response.text();
+
+      if (responseText === "1") {
+        form.textContent = "Te has suscrito con éxito 🎉";
+        if (form instanceof HTMLFormElement) {
+          form.reset();
+        } else {
+          console.error("The provided element is not a valid form.");
+        }
+      } else if (responseText === "Already subscribed.") {
+        messageContainer.textContent =
+          "Parece que ya te habías suscrito antes 🤔";
+      } else {
+        messageContainer.textContent = `Error: ${responseText}`;
+      }
+    }
+  } catch (e) {
+    console.error("Error subscribing user:", e);
+    alert("Ocurrió un error al suscribirte, intenta mas tarde");
+  }
+});
---
+++ src/utils/numbers.ts
@@ -0,0 +1,23 @@
+export function formatSubscribersCount(count: number): string {
+  if (count < 1000) {
+    return count.toString();
+  } else if (count < 1000000) {
+    return (count / 1000).toFixed(1).replace(/\.0$/, "") + "K";
+  } else {
+    return (count / 1000000).toFixed(1).replace(/\.0$/, "") + "M";
+  }
+}
+
+export async function getFormattedSubscribersCount(
+  value: Promise<string>
+): Promise<string | null> {
+  try {
+    const count = await value;
+    const countNumber = parseInt(count, 10);
+    console.log(countNumber);
+    return formatSubscribersCount(countNumber);
+  } catch (e) {
+    console.error("Error resolving promise", e);
+    return null;
+  }
+}
