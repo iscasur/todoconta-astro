8f9e6b3ac6d00d513d3ca8494edacbb843a81e71
Israel Castro <israel.castro@gmail.com>
2024-11-17 02:00:24 -0600

    Refactor styles and update subscription form API references
    Adjusted CSS for better responsive design, and externalized API keys and URLs to environment variables for the subscription form. Also improved subscription message handling and added site configuration details.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,19 @@ import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://todoconta.com",
   integrations: [tailwind(), partytown({
     config: {
       forward: ["dataLayer.push"],
     }
-  })]
+  })],
+  experimental: {
+    env: {
+      schema: {
+        API_URL_SENDY: envField.string({context: "client", access: "public", default: "https://sendy.todoconta.com"}),
+        API_KEY_SENDY: envField.string({context: "client", access: "public", default: "5a7Ijeup4Dmx0S3QghQ3"}),
+        LIST_ID_SENDY: envField.string({context: "client", access: "public", default: "k10AD2h1Bu4fTfy763prrdpA"})
+      }
+    }
+  }
 });
