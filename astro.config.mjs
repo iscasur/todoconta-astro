b2c77b73c1ddf0bd48429449387eabc258a3b220
Israel Castro <israel.castro@gmail.com>
2024-11-20 14:09:38 -0600

    Add AboutMe and Carousel components; update layout and styles
    Integrated new 'AboutMe' and 'Carousel' components for improved content display. Updated 'Testimonials', 'Services', and 'BlogHome' sections to use Carousel. Enhanced 'Hero', 'Contact', and global styles for better usability and design consistency. Removed redundant footer flag in layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,8 @@ export default defineConfig({
       schema: {
         API_URL_SENDY: envField.string({context: "client", access: "public", default: "https://sendy.todoconta.com"}),
         API_KEY_SENDY: envField.string({context: "client", access: "public", default: "5a7Ijeup4Dmx0S3QghQ3"}),
-        LIST_ID_SENDY: envField.string({context: "client", access: "public", default: "k10AD2h1Bu4fTfy763prrdpA"})
+        LIST_ID_SENDY: envField.string({context: "client", access: "public", default: "k10AD2h1Bu4fTfy763prrdpA"}),
+        WA_NUMBER: envField.string({context: "client", access: "public", default: "5215544753602"})
       }
     }
   }
