c63934d4ea8b96a25c33c3a5d046255c1a2090d0
Israel Castro <israel.castro@gmail.com>
2024-11-26 23:14:14 -0600

    Add URL redirects for product and demo links
    Introduced new URL redirects in `astro.config.mjs` for various product pages and demo versions. This improves navigational efficiency and user experience by directly mapping short URLs to their respective detailed or external resource links.

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,27 @@ export default defineConfig({
       forward: ["dataLayer.push"],
     }
   })],
+  redirects: {
+    '/xmlsatpremium': '/producto/xmlsat-premium/',
+    '/xmlsat': '/producto/xmlsat/',
+    '/controlxml': '/producto/control-xml-cfdi/',
+    '/controlxmlnom': '/producto/control-xml-nomina/',
+    '/conciliarep': '/producto/conciliarep/',
+    '/xmlctpqi': '/producto/xml-a-contpaqi/',
+    '/csd': '/certificado-sello-digital-csd',
+    '/wss': '/web-service-sat/',
+    '/dp': '/deducciones-personales/',
+    '/fe': '/firma-electronica/',
+    '/ie': '/ingresos-gravados-exentos-isr/',
+    '/xmlctpqi-demo': 'https://bit.ly/2Ly9ioI',
+    '/xmlcontpaq-demo': 'https://bit.ly/2Ly9ioI',
+    '/controlxml-demo': 'https://bit.ly/3q6Orb4',
+    '/controlxmlnom-demo': 'https://bit.ly/3rFHC0q',
+    '/conciliarep-demo': 'https://bit.ly/37eQ2ED',
+    '/xmlsatpremium-demo': 'http://softwarepaq2.com/downloads/software/SetupXMLSAT_PREMIUM.exe',
+    '/xmlsat-demo': 'http://softwarepaq2.com/downloads/software/SetupXMLSAT.exe',
+    '/alegra': 'https://app.alegra.com/user/register/country/mexico?coupon=CNTODOCONTA'
+  },
   experimental: {
     env: {
       schema: {
